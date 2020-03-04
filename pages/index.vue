4face22f44721a780df5d38b5bf28adead11ab8c
Alexander Hung <hung9762@gmail.com>
2020-03-04 00:42:28 -0600

    Adding a small bio

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,76 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        Alexander Hung
+        Hello.
       </h1>
-      <h2 class="subtitle">
-        
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p class="subtitle">
+        My name is Alexander Hung
+      </p>
+      <p>
+        I am an independent software developer, I've worked as an AQA in the past, but right now I am learning web and app development.
+      </p>
+      <p>
+        My main goal from this website, is to create a blog to share my projects. Right now I am working on an AQA Testing Environment using Java and this website using Vue.js and Nuxt.js. I will add my github account ASAP I get to a stable version of the environment.
+      </p> 
+      <p>
+        Outside of programming and university. I enjoy playing indie games, learning languages and drawing.
+      </p>
+      <p>
+        If you happen to speak Spanish, Deutsch (Entry level) or 中文, you can get in contact by writing to hung9762@gmail.com
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+//import Logo from '~/components/Logo.vue'
 
 export default {
+  data () {
+    return {
+      title: 'Alexander Hung'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      meta: [
+        { hid: 'description', name: 'description', content: 'My custom description'}
+      ]
+    }
+  },
   components: {
-    Logo
+    //Logo
   }
 }
 </script>
 
 <style>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  font-family: 'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-size: calc(10px + 0.33vw);
+  -webkit-font-smoothing: antialiased;
+  padding: 5vh 10vw;
+  color: #121314;
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+  font-size: 4.5em;
+  font-weight: 450;
+  margin-bottom: 0;
 }
 
 .subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+  font-weight:300;
+  font-size:2.5em
+}
+
+p {
+  font-size: 1.6em;
+  font-weight: 250;
+  line-height: 1.4;
+  max-width: 26em;
 }
 
 .links {
